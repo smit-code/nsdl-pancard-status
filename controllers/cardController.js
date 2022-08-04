334268ed6bcc10b7a03f100b06f9d88bd1a8c3da
Smit Gajera <smitgajera6@gmail.com>
2022-08-04 13:03:41 +0530

    Update cardController.js

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -120,7 +120,7 @@ exports.deleteCard = async (req, res, next) => {
 exports.getAllCardStatus = async (req, res) => {
     console.log("HITTING")
 
-    const cards = await Card.find({is_synced:0, is_dispatched: 0}).sort("id").limit(12);
+    const cards = await Card.find({is_synced:0, is_dispatched: 0}).sort("id").limit(2);
     if (!cards) {
         console.log('Cards not found')
     }
